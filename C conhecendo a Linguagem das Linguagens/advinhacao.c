842df46e7dcf77f402bea4fea557ec8fb2be93f7
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2024-04-05 21:31:30 -0400

    Adicionei o uso dos comandos scanf e como declarar  um variável inteira

--- C conhecendo a Linguagem das Linguagens/advinhacao.c
+++ C conhecendo a Linguagem das Linguagens/advinhacao.c
@@ -7,7 +7,16 @@ int main() {
     UINT CPAGE_DEFAULT = GetConsoleOutputCP();
     SetConsoleOutputCP(CPAGE_UTF8);
 
-    printf("Bem vindo ao nosso jogo de advinhação\n");
+    printf("*****************************************\n");
+    printf("* Bem vindo ao nosso jogo de advinhação *\n");
+    printf("*****************************************\n");
+
+    int numeroSecreto = 42;
+    int chute;
+
+    printf("Qual é o seu chute? ");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d", chute);
 
     SetConsoleOutputCP(CPAGE_DEFAULT);
     return 0;
