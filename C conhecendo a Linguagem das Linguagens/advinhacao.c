eedb335516c721b86baa8c704c91cf3534a3c597
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2024-05-24 17:48:24 -0400

    Refatoração e correção do código

--- C conhecendo a Linguagem das Linguagens/advinhacao.c
+++ C conhecendo a Linguagem das Linguagens/advinhacao.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <windows.h>
 
-#define NUMERO_DE_TENTATIVAS 5
-
 int main() {
     //Como colocar acentuação no windows
     UINT CPAGE_UTF8 = 65001;
@@ -15,37 +13,38 @@ int main() {
 
     int numeroSecreto = 42;
     int chute;
+    int tentativas = 1;
     
-    for(int i = 1; i <= NUMERO_DE_TENTATIVAS; i++){
-        
-        printf("Tetantiva %d de %d\n", i, NUMERO_DE_TENTATIVAS);
+    while(1){
+        printf("Tentativa %d\n", tentativas);
         printf("Qual é o seu chute? ");
         scanf("%d", &chute);
         printf("Seu chute foi %d\n", chute);
 
-        if (chute > 0){
+        if (chute < 0){
             printf("Você não pode chutar números negativos!\n");
-            i--; //Vai diminuir essa tentativa, pois as tenativas válidas são apenas as com números positivos
-
             continue; //Faz o código parar nessa parte e ir direto para o próximo loop
         }
 
         int acertou =(chute == numeroSecreto);
         int maior = (chute > numeroSecreto);
-        int menor = (chute < numeroSecreto);
 
         if(acertou){
             printf("Parabêns! Você acertou!\n");
             printf("Você é bom nisso! Jogue outra vez! \n");
-            break; //Quebra o loop
+            break;
         }
         else if(maior){
             printf("O seu chute foi maior que o número secreto!\n");
         }
-        else if(menor){
+        else{
             printf("O seu chute foi menor que o número secreto!\n");
         }
+        tentativas ++;
     }
+
+    printf("FIM DE JOGO!\n");
+    printf("Você acertou em %d tentativas!", tentativas);
 
     SetConsoleOutputCP(CPAGE_DEFAULT);
     return 0;
