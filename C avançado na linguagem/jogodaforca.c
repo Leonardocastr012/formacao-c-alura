ff456a15dd509a05afe7493bc97aa38ff307ecbf
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2024-06-04 17:14:59 -0400

    Criação do arquivo que vai ficar o jogo da forca

---
+++ C avançado na linguagem/jogodaforca.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    //Como colocar acentuação no windows
+    UINT CPAGE_UTF8 = 65001;
+    UINT CPAGE_DEFAULT = GetConsoleOutputCP();
+    SetConsoleOutputCP(CPAGE_UTF8);
+
+   
+    SetConsoleOutputCP(CPAGE_DEFAULT);
+    return 0;
+}
