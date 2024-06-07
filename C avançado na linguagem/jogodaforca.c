07eabc4b17ff89264ba5cecf198e5d8436ef90ad
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2024-06-07 17:39:12 -0400

    Percorrendo o array

--- C avançado na linguagem/jogodaforca.c
+++ C avançado na linguagem/jogodaforca.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <windows.h>
 
 int main (){
@@ -14,7 +15,15 @@ int main (){
     int enforcou = 0;
 
     do {
-        //começar o nosso jogo
+        char chute;
+        scanf("%c", &chute);
+
+        for(int i = 0; i < strlen(palavrasecreta); i++){
+            if(palavrasecreta[i] == chute){
+                printf("A posição %d tem essa letra!\n", i);
+            }
+        }
+
     } while(!acertou && !enforcou);//Vai tá dando 1 em ambas variaveis então o loop vai tá funcionando pois 1 é True
     
     SetConsoleOutputCP(CPAGE_DEFAULT);
